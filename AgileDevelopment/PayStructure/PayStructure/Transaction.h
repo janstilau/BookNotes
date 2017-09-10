bbc229347e0e7a3784317648a347824281d3e9b2
justin lau <janstilau@gmail.com>
2017-09-10 23:07:41 +0800

    sync

---
+++ AgileDevelopment/PayStructure/PayStructure/Transaction.h
@@ -0,0 +1,15 @@
+//
+//  Transaction.h
+//  PayStructure
+//
+//  Created by justin lau on 2017/9/10.
+//  Copyright © 2017年 jansti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Transaction : NSObject
+
+- (void)execute;
+
+@end
---
+++ AgileDevelopment/PayStructure/PayStructure/Transaction.m
@@ -0,0 +1,17 @@
+//
+//  Transaction.m
+//  PayStructure
+//
+//  Created by justin lau on 2017/9/10.
+//  Copyright © 2017年 jansti. All rights reserved.
+//
+
+#import "Transaction.h"
+
+@implementation Transaction
+
+- (void)execute {
+    
+}
+
+@end
